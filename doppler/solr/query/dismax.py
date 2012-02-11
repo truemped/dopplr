556fba67f9a70edab1337b0a29ee8d483ac77328
Daniel Truemper <truemped@googlemail.com>
2012-02-11 21:59:01 +0100

    the (e)dismax query

---
+++ doppler/solr/query/dismax.py
@@ -0,0 +1,64 @@
+# vim: set fileencoding=utf-8 :
+#
+# Copyright (c) 2011 Daniel Truemper <truemped at googlemail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+"""
+The Solr (e)DisMax query.
+
+For more information see: http://wiki.apache.org/solr/DisMaxQParserPlugin
+"""
+from doppler.basequery import BaseQuery
+
+
+class DisMax(BaseQuery):
+    """
+    The dismax query.
+    """
+
+    def __init__(self, qf, alt=None, mm=None, pf=None, ps=None, qs=None,
+            tie=None, bq=None, bf=None, edismax=True):
+        """
+        Initialize the query values.
+        """
+        self.__qf = qf
+        if edismax:
+            self.__deftype = 'edismax'
+        else:
+            self.__deftype = 'dismax'
+        self.__optional_params = {
+            'q.alt': alt,
+            'mm': mm,
+            'pf': pf,
+            'ps': ps,
+            'qs': qs,
+            'tie': tie,
+            'bq': bq,
+            'bf': bf
+        }
+
+    def get_params(self):
+        """
+        Return the list of query params.
+        """
+        params = []
+        params.append(('defType', self.__deftype))
+        params.append(('qf', self.__qf))
+
+        for p in self.__optional_params:
+            if self.__optional_params[p]:
+                params.append((p, self.__optional_params[p]))
+
+        return params
---
+++ vows/dismax_vows.py
@@ -0,0 +1,124 @@
+# vim: set fileencoding=utf-8 :
+#
+# Copyright (c) 2011 Daniel Truemper <truemped at googlemail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+from pyvows import Vows, expect
+
+from doppler.solr.query.dismax import DisMax
+
+
+@Vows.batch
+class WithTheDisMax(Vows.Context):
+
+    class WithDefaultParams(Vows.Context):
+
+        def topic(self):
+            q = DisMax('title^2 body^1', edismax=False)
+            return q.get_params()
+
+        def theDefTypeMustMatch(self, topic):
+            expect(topic).to_include(('defType', 'dismax'))
+
+        def theQueryFieldsMatch(self, topic):
+            expect(topic).to_include(('qf', 'title^2 body^1'))
+
+        def noUnexpectedArgs(self, topic):
+            expect(topic).to_length(2)
+
+    class WithOptionalParameters(WithDefaultParams):
+
+        def topic(self):
+            valid_params = [
+                'alt',
+                'mm',
+                'pf',
+                'ps',
+                'qs',
+                'tie',
+                'bq',
+                'bf'
+            ]
+
+            for v in valid_params:
+                kwargs = {v: 'a'}
+                q = DisMax('title^2 body^1', edismax=False, **kwargs)
+                yield (v, q.get_params())
+
+        def theDefTypeMustMatch(self, (field, params)):
+            expect(params).to_include(('defType', 'dismax'))
+
+        def theQueryFieldsMatch(self, (field, params)):
+            expect(params).to_include(('qf', 'title^2 body^1'))
+
+        def noUnexpectedArgs(self, (field, params)):
+            expect(params).to_length(3)
+
+        def theQueryMustMatch(self, (field, params)):
+            if field == 'alt':
+                field = 'q.alt'
+            expect(params).to_include((field, 'a'))
+
+
+@Vows.batch
+class WithTheEDisMax(Vows.Context):
+
+    class WithDefaultParams(Vows.Context):
+
+        def topic(self):
+            q = DisMax('title^2 body^1')
+            return q.get_params()
+
+        def theDefTypeMustMatch(self, topic):
+            expect(topic).to_include(('defType', 'edismax'))
+
+        def theQueryFieldsMatch(self, topic):
+            expect(topic).to_include(('qf', 'title^2 body^1'))
+
+        def noUnexpectedArgs(self, topic):
+            expect(topic).to_length(2)
+
+    class WithOptionalParameters(WithDefaultParams):
+
+        def topic(self):
+            valid_params = [
+                'alt',
+                'mm',
+                'pf',
+                'ps',
+                'qs',
+                'tie',
+                'bq',
+                'bf'
+            ]
+
+            for v in valid_params:
+                kwargs = {v: 'a'}
+                q = DisMax('title^2 body^1', **kwargs)
+                yield (v, q.get_params())
+
+        def theDefTypeMustMatch(self, (field, params)):
+            expect(params).to_include(('defType', 'edismax'))
+
+        def theQueryFieldsMatch(self, (field, params)):
+            expect(params).to_include(('qf', 'title^2 body^1'))
+
+        def noUnexpectedArgs(self, (field, params)):
+            expect(params).to_length(3)
+
+        def theQueryMustMatch(self, (field, params)):
+            if field == 'alt':
+                field = 'q.alt'
+            expect(params).to_include((field, 'a'))
