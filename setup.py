7639831c6387e8a34b9372365839d95d794d2166
Daniel Truemper <truemper@retresco.de>
2012-11-20 11:30:48 +0100

    Travis CI

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@
 from setuptools import setup, find_packages
 
 
-tests_require = ['coverage', 'tornado-pyvows>=0.3.1']
+tests_require = ['coverage', 'tornado-pyvows>=0.5.0']
 
 
 setup(
-    name = "doppler",
-    version = "0.1.0",
-    description = "Doppler is a Tornado based client library for Solr and ElasticSearch",
+    name = "dopplr",
+    version = "0.8.0",
+    description = "Dopplr is a Tornado based client library for Solr and ElasticSearch",
     author = "Daniel Truemper",
     author_email = "truemped@googlemail.com",
-    url = "http://truemped.github.com/doppler",
+    url = "http://truemped.github.com/dopplr",
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
-        'tornado',
+        'tornado>=2.1',
     ],
     tests_require = tests_require,
     extras_require = {'test': tests_require}
